08da8f42d3ea9e4d2b554432a906fa241777414c
Olkhonwind <hadahanov80@gmail.com>
2023-01-12 20:30:53 +0800

    Initial commit

---
+++ venv/unit 12.7.3.py
@@ -0,0 +1,11 @@
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+money = float(input('сумма вклада: '))
+
+deposit = []
+
+deposit.append(int(money * per_cent['ТКБ']/100))
+deposit.append(int(money * per_cent['СКБ']/100))
+deposit.append(int(money * per_cent['ВТБ']/100))
+deposit.append(int(money * per_cent['СБЕР']/100))
+
+print("Максимальная сумма, которую вы можете заработать —", max(deposit))
